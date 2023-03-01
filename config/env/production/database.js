5dda1d838d4892a331dc6a970b3e4db58b2bc78f
SeferX <seferibrahim2@gmail.com>
2023-03-01 14:51:30 +0300

    Update production database.js file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST"),
       port: env("DATABASE_PORT"),
       database: env("DATABASE_NAME"),
-      user: env("DATABASE_USER"),
+      user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
       ssl: env.bool('DATABASE_SSL', false),
     },
