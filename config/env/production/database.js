aed8c85197368bddd8712f07c338c149dad5d55f
SeferX <seferibrahim2@gmail.com>
2023-03-17 15:05:50 +0300

    Debug logic added

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,27 +1,16 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-      },
-      options: {},
-    },
-  },
-  // Use dynamic connection objects based on the subdomain
-  functions: {
-    database: {
-      connections: {},
+  connection: {
+    client: "mysql",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env("DATABASE_PORT"),
+      database: env(`DATABASE_NAME`),
+      user: env(`DATABASE_USERNAME`),
+      password: env(`DATABASE_PASSWORD`),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
-
 
 // const parse = require("pg-connection-string").parse;
 // const config = parse(process.env.DATABASE_URL);
--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -8,33 +8,45 @@ module.exports = (config, { strapi }) => {
         : subdomain.toString().toUpperCase() + "_DATABASE"
     }`;
 
-    console.log(host);
-    console.log(subdomain);
-    console.log(envPrefix);
+    strapi.log.debug(host);
+    strapi.log.debug(subdomain);
+    strapi.log.debug(envPrefix);
 
     const dbUser = process.env[`${envPrefix}_USERNAME`];
     const dbPassword = process.env[`${envPrefix}_PASSWORD`];
     const dbName = process.env[`${envPrefix}_NAME`];
 
-    console.log(dbUser);
-    console.log(dbPassword);
-    console.log(dbName);
+    strapi.log.debug(dbUser);
+    strapi.log.debug(dbPassword);
+    strapi.log.debug(dbName);
 
-    const connections = strapi.config.get("database.connections", {});
-    const defaultConnection = strapi.config.get(
-      "database.defaultConnection",
-      "default"
-    );
-    const defaultSettings = connections[defaultConnection];
-    const dbSettings = Object.assign({}, defaultSettings, {
+    // const connections = strapi.config.get("database.connections", {});
+    // const defaultConnection = strapi.config.get(
+    //   "database.defaultConnection",
+    //   "default"
+    // );
+    // const defaultSettings = connections[defaultConnection];
+    // const dbSettings = Object.assign({}, defaultSettings, {
+    //   database: dbName,
+    //   user: dbUser,
+    //   password: dbPassword,
+    // });
+    // connections[defaultConnection] = dbSettings;
+    // strapi.config.set({ database: { connections } });
+
+    const dbSettings = {
       database: dbName,
       user: dbUser,
       password: dbPassword,
-    });
-    connections[defaultConnection] = dbSettings;
-    strapi.config.set({ database: { connections } });
+    };
 
-    console.log(strapi.db.config.connection);
+    strapi.db.config.connection.connection = {
+      ...strapi.db.config.connection.connection,
+      ...dbSettings,
+    };
+
+    console.log(strapi.db.config.connection.connection);
+    strapi.log.debug(strapi.db.config.connection.connection);
 
     // Call the next middleware in the chain
     await next();
