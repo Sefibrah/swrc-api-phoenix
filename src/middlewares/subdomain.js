bd6fb34b93e1ebfdfa82d76db036daa70f53c8d9
SeferX <seferibrahim2@gmail.com>
2023-03-17 09:41:04 +0300

    Add new subdomain shit

--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -1,11 +1,26 @@
-module.exports = (strapi) => {
+module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const host = ctx.request.header.host;
     const subdomain = host.split(".")[0];
 
-    const database = `${subdomain}_db`;
+    const prefix = subdomain.toString().toUpperCase();
 
-    strapi.connections.default.settings.database = database;
+    if (prefix.toLowerCase() != "localhost:1337") {
+      const dbConfig = {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env[`${prefix}_DB_NAME`],
+        user: process.env[`${prefix}_DB_USERNAME`],
+        password: process.env[`${prefix}_DB_PASSWORD`],
+        ssl: false,
+      };
+
+      const currentConfig = strapi.db.config.connection.connection;
+      strapi.db.config.connection.connection = {
+        ...currentConfig,
+        ...dbConfig,
+      };
+    }
 
     // Call the next middleware in the chain
     await next();
