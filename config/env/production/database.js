accf79099aa1562fbe27a668fec072b704d98cc6
SeferX <seferibrahim2@gmail.com>
2023-03-17 15:47:31 +0300

    Attempt 20 million billion

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,29 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST"),
-      port: env("DATABASE_PORT"),
-      database: env(`DATABASE_NAME`),
-      user: env(`DATABASE_USERNAME`),
-      password: env(`DATABASE_PASSWORD`),
-      ssl: env.bool("DATABASE_SSL", false),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+      },
+      options: {},
+    },
+    gulftravelbosnia: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("GULFTRAVELBOSNIA_DATABASE_NAME", "strapi"),
+        username: env("GULFTRAVELBOSNIA_DATABASE_USERNAME", "strapi"),
+        password: env("GULFTRAVELBOSNIA_DATABASE_PASSWORD", "strapi"),
+      },
+      options: {},
     },
   },
 });
--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -2,23 +2,27 @@ module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const host = ctx.request.header.host;
     const subdomain = host.split(".")[0];
-    const envPrefix = `${
-      host.split(".").length === 1
-        ? "DATABASE"
-        : subdomain.toString().toUpperCase() + "_DATABASE"
-    }`;
+    // const envPrefix = `${
+    //   host.split(".").length === 1
+    //     ? "DATABASE"
+    //     : subdomain.toString().toUpperCase() + "_DATABASE"
+    // }`;
 
-    strapi.log.debug(host);
-    strapi.log.debug(subdomain);
-    strapi.log.debug(envPrefix);
+    // strapi.log.debug(host);
+    // strapi.log.debug(subdomain);
+    // strapi.log.debug(envPrefix);
 
-    const dbUser = process.env[`${envPrefix}_USERNAME`];
-    const dbPassword = process.env[`${envPrefix}_PASSWORD`];
-    const dbName = process.env[`${envPrefix}_NAME`];
+    // const dbUser = process.env[`${envPrefix}_USERNAME`];
+    // const dbPassword = process.env[`${envPrefix}_PASSWORD`];
+    // const dbName = process.env[`${envPrefix}_NAME`];
 
-    strapi.log.debug(dbUser);
-    strapi.log.debug(dbPassword);
-    strapi.log.debug(dbName);
+    // strapi.log.debug(dbUser);
+    // strapi.log.debug(dbPassword);
+    // strapi.log.debug(dbName);
+
+    if (subdomain === 'gulftravelbosnia') {
+      strapi.connections.default = strapi.connections.gulftravelbosnia;
+    }
 
     // const connections = strapi.config.get("database.connections", {});
     // const defaultConnection = strapi.config.get(
@@ -34,19 +38,19 @@ module.exports = (config, { strapi }) => {
     // connections[defaultConnection] = dbSettings;
     // strapi.config.set({ database: { connections } });
 
-    const dbSettings = {
-      database: dbName,
-      user: dbUser,
-      password: dbPassword,
-    };
+    // const dbSettings = {
+    //   database: dbName,
+    //   user: dbUser,
+    //   password: dbPassword,
+    // };
 
-    strapi.db.config.connection.connection = {
-      ...strapi.db.config.connection.connection,
-      ...dbSettings,
-    };
+    // strapi.db.config.connection.connection = {
+    //   ...strapi.db.config.connection.connection,
+    //   ...dbSettings,
+    // };
 
-    console.log(strapi.db.config.connection.connection);
-    strapi.log.debug(strapi.db.config.connection.connection);
+    // console.log(strapi.db.config.connection)
+    // strapi.log.debug(strapi.db.config.connection);
 
     // Call the next middleware in the chain
     await next();
