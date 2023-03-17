8ee814b8371df77344ce81e546631ebc11b5819d
SeferX <seferibrahim2@gmail.com>
2023-03-17 16:49:26 +0300

    Update all!

--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -1,17 +1,12 @@
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const subdomain = ctx.request.header.host.split(".")[0]; // get the subdomain from the request header
+    console.log(strapi.db.config.connections);
+    const connections = strapi.db.config.connections;
 
-    console.log(strapi.config.database);
-    if (
-      strapi.config.database?.connections != null &&
-      strapi.config.database?.connections[subdomain]
-    ) {
-      // if the database connection exists, set it as the current connection
-      strapi.config.currentEnvironment.database = subdomain;
-    } else {
-      // if the database connection does not exist, log an error
-      console.error(`Database connection ${subdomain} does not exist`);
+    // modify the default connection object based on the subdomain
+    if (subdomain === "gulftravelbosnia") {
+      connections.default = connections.gulftravelbosnia;
     }
     await next();
   };
