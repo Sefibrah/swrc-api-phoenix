d7b60485db404aaacf37439f12a4fba8282b3e1a
SeferX <seferibrahim2@gmail.com>
2023-03-17 10:39:43 +0300

    Update middlewares again...

--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -1,37 +1,24 @@
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
-
     const host = ctx.request.header.host;
     const subdomain = host.split(".")[0];
-    const envPrefix = `${subdomain.toString().toUpperCase()}_DATABASE` || 'DATABASE';
+    const envPrefix =
+      `${subdomain.toString().toUpperCase()}_DATABASE` || "DATABASE";
 
-    console.log(host);
-    console.log(subdomain);
-    console.log(envPrefix);
-    if (
-      host != "https://swrcapi.com" &&
-      envPrefix.toLowerCase() != "localhost:1337" &&
-      process.env.NODE_ENV === "production"
-    ) {
-      const dbUser = process.env[`${envPrefix}_USER`];
-      const dbPassword = process.env[`${envPrefix}_PASSWORD`];
-      const dbName = process.env[`${envPrefix}_NAME`];
+    const dbUser = process.env[`${envPrefix}_USER`];
+    const dbPassword = process.env[`${envPrefix}_PASSWORD`];
+    const dbName = process.env[`${envPrefix}_NAME`];
 
-      const settings = {
-        client: 'mysql',
-        connection: {
-          host: process.env.HOST,
-          port: process.env.PORT || 3306,
-          database: dbName,
-          user: dbUser,
-          password: dbPassword,
-          charset: 'utf8',
-          collation: 'utf8_general_ci',
-        },
-      };
+    const settings = {
+      database: dbName,
+      user: dbUser,
+      password: dbPassword,
+    };
 
-      await strapi.config.functions.database.set(settings);
-    }
+    strapi.db.config.connection.connection = {
+      ...strapi.db.config.connection.connection,
+      ...settings,
+    };
 
     // Call the next middleware in the chain
     await next();
