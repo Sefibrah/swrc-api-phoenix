a39ebf73ff4f5f34262f31563cefaaf5fa853215
SeferX <seferibrahim2@gmail.com>
2023-03-18 20:22:48 +0300

    update subdomain

--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -1,46 +1,27 @@
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const subdomain = ctx.request.header.host.split(".")[0]; // get the subdomain from the request header
-    console.log(strapi.db.config.connections);
     const connections = strapi.db.config.connections;
+    console.log("connections", strapi.db.config.connections);
+    console.log("subdomain", subdomain);
 
-    strapi
-      .plugin("sentry")
-      .service("sentry")
-      .sendError(`subdomain: ${subdomain}`, (scope, sentryInstance) => {
-        // Customize the scope here
-        scope.setTag("my_custom_tag", "Tag value");
-      });
-
-    strapi
-      .plugin("sentry")
-      .service("sentry")
-      .sendError(connections, (scope, sentryInstance) => {
-        // Customize the scope here
-        scope.setTag("my_custom_tag", "Tag value");
-      });
     // modify the default connection object based on the subdomain
     if (subdomain === "gulftravelbosnia") {
-      strapi
-        .plugin("sentry")
-        .service("sentry")
-        .sendError(connections.gulftravelbosnia, (scope, sentryInstance) => {
-          // Customize the scope here
-          scope.setTag("my_custom_tag", "Tag value");
-        });
+      console.log("connections.gulftravelbosnia", connections.gulftravelbosnia);
       strapi.db.config.connections.default = connections.gulftravelbosnia;
-
-      strapi
-        .plugin("sentry")
-        .service("sentry")
-        .sendError(
-          strapi.db.config.connections.default,
-          (scope, sentryInstance) => {
-            // Customize the scope here
-            scope.setTag("my_custom_tag", "Tag value");
-          }
-        );
+      console.log(
+        "strapi.db.config.connections.default",
+        strapi.db.config.connections.default
+      );
     }
+
+    strapi
+      .plugin("sentry")
+      .service("sentry")
+      .sendError("SEE WHAT IS GOING ON!!!", (scope, sentryInstance) => {
+        // Customize the scope here
+        scope.setTag("my_custom_tag", "Tag value");
+      });
     await next();
   };
 };
