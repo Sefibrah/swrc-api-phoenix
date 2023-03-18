824658acf34142ae051fa17ee0f3193d42d48c6c
SeferX <seferibrahim2@gmail.com>
2023-03-18 20:29:12 +0300

    Add better console logging for sentry

--- src/middlewares/subdomain.js
+++ src/middlewares/subdomain.js
@@ -2,23 +2,26 @@ module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const subdomain = ctx.request.header.host.split(".")[0]; // get the subdomain from the request header
     const connections = strapi.db.config.connections;
-    console.log("connections", strapi.db.config.connections);
+    console.log("connections", JSON.stringify(strapi.db.config.connections));
     console.log("subdomain", subdomain);
 
     // modify the default connection object based on the subdomain
     if (subdomain === "gulftravelbosnia") {
-      console.log("connections.gulftravelbosnia", connections.gulftravelbosnia);
+      console.log(
+        "connections.gulftravelbosnia",
+        JSON.stringify(connections.gulftravelbosnia)
+      );
       strapi.db.config.connections.default = connections.gulftravelbosnia;
       console.log(
         "strapi.db.config.connections.default",
-        strapi.db.config.connections.default
+        JSON.stringify(strapi.db.config.connections.default)
       );
     }
 
     strapi
       .plugin("sentry")
       .service("sentry")
-      .sendError("SEE WHAT IS GOING ON!!!", (scope, sentryInstance) => {
+      .sendError("SUBDOMAIN", (scope, sentryInstance) => {
         // Customize the scope here
         scope.setTag("my_custom_tag", "Tag value");
       });
