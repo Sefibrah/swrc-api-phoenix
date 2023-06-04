540f0a0f4bd81eda7e0d0afa904ab1161723c8ad
Sefibrah <seferibrahim2@gmail.com>
2023-06-04 20:00:54 +0300

    delete test tables

--- src/api/test-table/services/test-table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-table service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::test-table.test-table');
