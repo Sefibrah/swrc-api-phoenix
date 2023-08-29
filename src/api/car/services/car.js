d8c75f997b83377abc80d41e30b7b16c6eb18078
IBRAHIM SEFER <seferibrahim2@gmail.com>
2023-08-29 09:53:42 +0200

    Quick fix for relevant events list

--- src/api/car/services/car.js
+++ src/api/car/services/car.js
@@ -14,6 +14,7 @@ module.exports = createCoreService("api::car.car", ({ strapi }) => ({
   relevantEventsList: async (carId, subdomain) => {
     const loggedUserUserGroup = await getLoggedUserUserGroup(strapi, subdomain);
     const today = new Date();
+    today.setHours(0, 0, 0, 0);
     const epicEventQuery = {
       userGroup: loggedUserUserGroup.id,
       car: {
@@ -126,7 +127,6 @@ module.exports = createCoreService("api::car.car", ({ strapi }) => ({
         },
         where: epicEventQuery,
       });
-    console.log(carContracts, carReservations, carMaintenances);
 
     return [
       ...carContracts.map((event) => {
